9a0773ef4d0f8471c95e502c42e1a3e93f974c39
kennyrkun <kennyrmccormick+github@gmail.com>
2020-02-27 14:01:30 -0600

    update files

--- client-service-windows/Service1.cs
+++ client-service-windows/Service1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Management;
